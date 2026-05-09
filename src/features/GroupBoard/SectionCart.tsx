7478bcf331893c1855b2ca73492d9c7f4ba963c7
AmirrezaHanafi <amirreza1688@gmail.com>
2026-05-09 20:53:09 +0330

    [EDIT] color of group cart

--- src/features/GroupBoard/SectionCart.tsx
+++ src/features/GroupBoard/SectionCart.tsx
@@ -8,12 +8,9 @@ export default function SectionCart({ section }: SectionCartProps) {
   return (
     <Link
       to={`/board/${section}`}
-      className="flex-1 transition-all hover:scale-105 duration-300 group px-4 border-4 py-4 pb-6 min-h-40 flex flex-col justify-between items-center  text-center  backdrop-blur-sm border-white/30 bg-white/10 rounded-md"
+      className="flex-1 transition-all  hover:scale-105 duration-300 group px-4  py-4  min-h-50 flex flex-col justify-center items-center  text-center  backdrop-blur-sm border-white/30 bg-card rounded-md"
     >
       <span className="font-InterBold inline-block text-xl  ">{section}</span>
-      <span className="inline-block mt-4  text-lg  text-blue-600 group-hover:text-blue-400 transition-colors ">
-        مشاهده
-      </span>
     </Link>
   )
 }
