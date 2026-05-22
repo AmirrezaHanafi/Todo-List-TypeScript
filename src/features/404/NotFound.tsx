d8c629e64963def8bb8d9b0afae35f1cfe05bbce
AmirrezaHanafi <amirreza1688@gmail.com>
2026-05-22 18:48:16 +0330

    [ADD] back to home button in 404 page

--- src/features/404/NotFound.tsx
+++ src/features/404/NotFound.tsx
@@ -1,7 +1,12 @@
+import { Link } from 'react-router-dom'
+
 export default function NotFound() {
   return (
-    <div className="font-InterBold text-4xl text-center  flex items-center justify-center  mt-60">
-      404 Not found
+    <div className="font-InterBold text-4xl text-center gap-y-7   flex items-center flex-col justify-center  mx-30 p-4   mt-60">
+      <span className="inline-block">404 Not found</span>
+      <Link className="py-2 px-4 font-DanaMedium text-lg  transition-all bg-white/10 rounded-lg hover:bg-white/20 " to={'/'}>
+        برگشت به صفحه اصلی
+      </Link>
     </div>
   )
 }
